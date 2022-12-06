7074a49549ed81478fc7a9ac19969ed3a20abb56
rushi412406 <119650676+rushi412406@users.noreply.github.com>
2022-12-06 11:58:34 +0530

    Update snow.py

--- snow.py
+++ snow.py
@@ -23,7 +23,6 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 
-import requests
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
